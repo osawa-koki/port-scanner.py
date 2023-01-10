ae6549a560595000bd745d6943be9c7af7519bf0
osawa-koki <github@osawa.dev>
2023-01-10 20:49:26 +0900

    小さな修正。

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ start_port = 50
 end_port = 100
 
 for port in range(start_port, end_port+1):
+    print(f"Checking port {port}.")
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.settimeout(5)
     result = sock.connect_ex((host, port))
     if result == 0:
-        print(f"Port {port} is open")
+        print(f"🪟 Port {port} is open.")
     else:
-        print(f"Port {port} is closed")
+        print(f"🚪 Port {port} is closed.")
     sock.close()
