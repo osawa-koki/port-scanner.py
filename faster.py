9c7cab449d92f85ff5088abcd7aa4d02eb6bea7d
osawa-koki <github@osawa.dev>
2023-01-10 20:53:25 +0900

    小さな修正。

--- faster.py
+++ faster.py
@@ -6,6 +6,7 @@ start_port = 50
 end_port = 100
 
 def check_port(host, port):
+    print(f"Checking port {port}.")
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.settimeout(5)
     result = sock.connect_ex((host, port))
